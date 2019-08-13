82395ed430a1be72bea94443839a74a6e9c43b50
brandonmoore38 <53958951+brandonmoore38@users.noreply.github.com>
2019-08-12 21:14:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import plotly.graph_objs as go
 
 ########### Set up the chart
 
-myfavoritecolor='C74A2F'
-x_list=['giraffes', 'elephants', 'monkeys']
+myfavoritecolor='15F484'
+x_list=['velociraptor', 'allosaurus', 'camposaurus']
 y_list=y=[5, 2, 7]
 
 data = [go.Bar(
@@ -16,7 +16,7 @@ data = [go.Bar(
     )]
 
 layout = go.Layout(
-    title = 'My favorite animals', # Graph title
+    title = 'Petting Zoo', # Graph title
     xaxis = dict(title = 'Type of animal'), # x-axis label
     yaxis = dict(title = 'Number in the zoo'), # y-axis label
 
